af5cefd8526330cad8f9a25c28b345411ac5d932
Abir <abiirr10@gmail.com>
2025-01-27 13:22:06 +0600

    🐛 FIX: Credentials

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'pass123',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true,
+    ConfigModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => ({
+        type: config.get('DB_TYPE') as any,
+        host: config.get('DB_HOST') as string,
+        port: config.get('DB_PORT') as number,
+        username: config.get('DB_USERNAME') as string,
+        password: config.get('DB_PASSWORD') as string,
+        database: config.get('DB_DATABASE') as string,
+        autoLoadEntities: config.get('DB_AUTOLOAD_ENTITIES') === 'true',
+        synchronize: config.get('DB_SYNCHRONIZE') === 'true',
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
